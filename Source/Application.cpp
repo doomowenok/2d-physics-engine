45cefd3198ce287551b0661e95aaf695f92ce134
doomowenok <doomowenok@icloud.com>
2025-03-23 13:16:28 +0300

    Create particle on click.

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -12,9 +12,6 @@ void Application::Setup()
 {
     running = Graphics::OpenWindow();
 
-    Particle* smallBall = new Particle(50, 100, 1.0f, 4);
-    particles.push_back(smallBall);
-
     liquid.x = 0;
     liquid.y = Graphics::Height() / 2;
     liquid.w = Graphics::Width();
@@ -36,41 +33,50 @@ void Application::Input()
                 {
                     running = false;
                 }
-                if(event.key.keysym.sym == SDLK_UP)
+                if (event.key.keysym.sym == SDLK_UP)
                 {
                     pushForce.y = 50.0 * PIXELS_PER_METER;
                 }
-                if(event.key.keysym.sym == SDLK_RIGHT)
+                if (event.key.keysym.sym == SDLK_RIGHT)
                 {
                     pushForce.x = 50.0 * PIXELS_PER_METER;
                 }
-                if(event.key.keysym.sym == SDLK_DOWN)
+                if (event.key.keysym.sym == SDLK_DOWN)
                 {
                     pushForce.y = -50.0 * PIXELS_PER_METER;
                 }
-                if(event.key.keysym.sym == SDLK_LEFT)
+                if (event.key.keysym.sym == SDLK_LEFT)
                 {
                     pushForce.x = -50.0 * PIXELS_PER_METER;
                 }
                 break;
             case SDL_KEYUP:
-                if(event.key.keysym.sym == SDLK_UP)
+                if (event.key.keysym.sym == SDLK_UP)
                 {
                     pushForce.y = 0.0f;
                 }
-                if(event.key.keysym.sym == SDLK_RIGHT)
+                if (event.key.keysym.sym == SDLK_RIGHT)
                 {
                     pushForce.x = 0.0f;
                 }
-                if(event.key.keysym.sym == SDLK_DOWN)
+                if (event.key.keysym.sym == SDLK_DOWN)
                 {
                     pushForce.y = 0.0f;
                 }
-                if(event.key.keysym.sym == SDLK_LEFT)
+                if (event.key.keysym.sym == SDLK_LEFT)
                 {
                     pushForce.x = 0.0f;
                 }
                 break;
+            case SDL_MOUSEBUTTONDOWN:
+                if (event.button.button == SDL_BUTTON_LEFT)
+                {
+                    int x;
+                    int y;
+                    SDL_GetMouseState(&x, &y);
+                    Particle* particle = new Particle(x, y, 1.0f, 5);
+                    particles.push_back(particle);
+                }
         }
     }
 }
@@ -81,60 +87,58 @@ void Application::Update()
 
     int timeToWait = MILLISECONDS_PER_FRAME - (SDL_GetTicks() - timePreviousFrame);
 
-    if(timeToWait > 0)
+    if (timeToWait > 0)
     {
         SDL_Delay(timeToWait);
     }
 
-    float deltaTime = (float)(SDL_GetTicks() - timePreviousFrame) / MILLISECONDS_IN_SECOND;
+    float deltaTime = (float) (SDL_GetTicks() - timePreviousFrame) / MILLISECONDS_IN_SECOND;
 
-    if(deltaTime > 0.016)
+    if (deltaTime > 0.016)
     {
         deltaTime = 0.016;
     }
 
     timePreviousFrame = SDL_GetTicks();
 
-    Vec2 wind = Vec2(1.0f, 0.0f) * PIXELS_PER_METER;
+    // Vec2 wind = Vec2(1.0f, 0.0f) * PIXELS_PER_METER;
     Vec2 weight = Vec2(0.0f, 9.8f) * PIXELS_PER_METER;
 
-    for (Particle* particle : particles)
+    for (Particle* particle: particles)
     {
-        particle->AddForce(wind);
+        // particle->AddForce(wind);
         particle->AddForce(weight * particle->Mass);
         particle->AddForce(pushForce);
 
-        if(particle->Position.y >= liquid.y)
+        if (particle->Position.y >= liquid.y)
         {
             Vec2 drag = Force::GenerateDragForce(*particle, 0.01);
             particle->AddForce(drag);
         }
     }
 
-    for (Particle* particle : particles)
+    for (Particle* particle: particles)
     {
         particle->Integrate(deltaTime);
     }
 
-    for (Particle* particle : particles)
+    for (Particle* particle: particles)
     {
-        if(particle->Position.x - particle->Radius <= 0)
+        if (particle->Position.x - particle->Radius <= 0)
         {
             particle->Position.x = particle->Radius;
             particle->Velocity.x *= -0.9f;
-        }
-        else if(particle->Position.x + particle->Radius >= Graphics::Width())
+        } else if (particle->Position.x + particle->Radius >= Graphics::Width())
         {
             particle->Position.x = Graphics::Width() - particle->Radius;
             particle->Velocity.x *= -0.9f;
         }
 
-        if(particle->Position.y - particle->Radius <= 0)
+        if (particle->Position.y - particle->Radius <= 0)
         {
             particle->Position.y = particle->Radius;
             particle->Velocity.y *= -0.9f;
-        }
-        else if(particle->Position.y + particle->Radius >= Graphics::Height())
+        } else if (particle->Position.y + particle->Radius >= Graphics::Height())
         {
             particle->Position.y = Graphics::Height() - particle->Radius;
             particle->Velocity.y *= -0.9f;
@@ -148,9 +152,9 @@ void Application::Render()
 
     Graphics::DrawFillRect(liquid.x + liquid.w / 2, liquid.y + liquid.h / 2, liquid.w, liquid.h, 0xFF6E3713);
 
-    for (Particle* particle : particles)
+    for (Particle* particle: particles)
     {
-        Graphics::DrawFillCircle((int)particle->Position.x, (int)particle->Position.y, particle->Radius, 0xFFFFFFFF);
+        Graphics::DrawFillCircle((int) particle->Position.x, (int) particle->Position.y, particle->Radius, 0xFFFFFFFF);
     }
 
     Graphics::RenderFrame();
@@ -158,7 +162,7 @@ void Application::Render()
 
 void Application::Destroy()
 {
-    for (Particle* particle : particles)
+    for (Particle* particle: particles)
     {
         delete particle;
     }
